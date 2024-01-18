1ce81d3c72dc442632caafa8b13a64441dd51334
Matheus Gomes Montovani <matheusgmontovani@gmail.com>
2024-01-18 10:29:54 -0300

    Atulization

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+/*function completeName(nome: string, sobrenome: string) {
+    return 'Welcome ' + nome + ' ' + sobrenome;
+}
+
+console.log(completeName('Matheus', 'Gomes Montovani'));
+
+function soma(number1: number, number2: number): number {
+    return number1 + number2;
+};
+
+function result() {
+    var number1 =  parseFloat((<HTMLInputElement>document.getElementById('number1')).value);
+    var number2 =  parseFloat((<HTMLInputElement>document.getElementById('number2')).value);
+
+    var resultado = soma(number1, number2);
+
+    (<HTMLInputElement>document.getElementById('result')).value = resultado.toString();
+
+    console.log('Resultado: ' + resultado);
+};*/
+
+
